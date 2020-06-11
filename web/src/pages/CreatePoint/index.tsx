dd4c8bbe5f4f318744f8805686dfabf9ceb88f0f
iara.oliveira <iara99oliveira@gmail.com>
2020-06-11 00:26:52 -0300

    adiciona o mapa e os selects buscando cidades e estado da api do ibge

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import api from '../../services/api'
+import axios from 'axios'
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 
+interface Item {
+    id: number;
+    title: string;
+    image: string;
+}
+
+interface IBGEUFResponse {
+    sigla: string;
+}
+
+interface IBGECityResponse {
+    nome: string;
+}
+
 const CreatePoint = () => {
+
+    // ao atribuir um estado para um array ou objeto, precisamos declarar manualmente seu type
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+    
+    const [selectedUf, setSelectedUf] = useState('0');
+
+    //função que será executada pelo componente da primeira vez que ele for carregado em uma tela
+    useEffect(()=>{
+        api.get('items').then(response => {
+            setItems(response.data);
+        });
+    }, []);
+    
+    useEffect(()=>{
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados/').then(response => {
+            const ufInicials = response.data.map(uf => uf.sigla);
+            setUfs(ufInicials)
+        });
+    }, []);
+
+    useEffect(()=>{
+        //carregar as cidade conforme a UF sempre que o valor ds selectedUf mudar
+        if(selectedUf === '0') {
+            return;
+        }
+
+        axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response => {
+            const cityNames = response.data.map(city => city.nome);
+            setCities(cityNames)
+        });
+
+    }, [selectedUf]);
+    
+    
+    //armazena o valor da UF selecionada
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+        const uf = event.target.value;
+        setSelectedUf(uf);
+    }
+
     return(
         <div id="page-create-point">
             <header>
@@ -71,14 +130,29 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select 
+                            name="uf" 
+                            id="uf" 
+                            value={selectedUf} 
+                            onChange={handleSelectUf}
+                            >
                                 <option value="0">Selecione uma UF</option>
+                                { 
+                                    ufs.map(uf => (
+                                        <option key={uf} value={uf}>{uf}</option>
+                                    ))
+                                }
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
                             <select name="city" id="city">
                                 <option value="0">Selecione uma cidade</option>
+                                { 
+                                    cities.map(city => (
+                                        <option key={city} value={city}>{city}</option>
+                                    ))
+                                }
                             </select>
                         </div>
                     </div>
@@ -91,30 +165,12 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Óleo de cozinha"/>
-                            <span>Óleo de Cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id} >
+                                <img src={item.image} alt={item.title}/>
+                                <span>{item.title}</span>
+                            </li >
+                        ))}
                     </ul>
                 </fieldset>
 
---
+++ web/src/services/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+// a biblioteca axios permite que façamos requisições ao backend
+
+const api = axios.create({
+    baseURL: 'http://localhost:3333'
+});
+
+export default api;
